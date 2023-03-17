452abca5a0612ab2eca32cd796724c8154eac433
marwan ali <marwanali.ma10@gmail.com>
2023-03-17 02:22:35 +0200

    sss

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 /**
  *  main - it prints a line using puts built in function
- * if zero is returned this mean that the main function got excuted (Success) */
-int main(void){
-	puts("\"Programming is like building a multilingual puzzle");
-	return 0;
+ * if zero returned this mean that the main function got excuted (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
