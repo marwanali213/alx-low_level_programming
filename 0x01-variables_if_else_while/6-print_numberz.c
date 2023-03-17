b29172230df28aace6e529bb8d6692cf163e5480
marwan ali <marwanali.ma10@gmail.com>
2023-03-17 15:41:38 +0200

    tasks all done

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - entry point
+ * description : same thing but with 
+ * Return: 0 (Success)
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	putchar(i + '0');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+	char c;
+	for (i = 0; i < 10; i++)
+	{
+	c = i + '0';
+	putchar(c);
+	}
+	for (i = 0; i < 6; i++)
+	{
+	c = i + 'a';
+	putchar(c);
+	}
+	putchar('\n');
+
+	return (0);
+}
