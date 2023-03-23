355b19906d874adf07cfa1acf21142592db6f364
Marwan Ali <96815791+marwanali213@users.noreply.github.com>
2023-03-23 04:14:31 +0200

    Update 1-alphabet.c

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,19 +1,21 @@
 #include "main.h"
 
 /**
- * print_alphabet - a function that prints the alphabet,
- * in lower case followed by a new line
- * Return: void
+ * print_alphabet - check the code for ALX School students.
+ *
+ * Return: Always 0.
  */
+
 void print_alphabet(void)
 {
-	char letter = 'a';
+	char alp = 'a';
 
-	while (letter <= 'z')
+	while (alp <= 'z')
 	{
-		_putchar(letter);
-		letter++;
-	}
+		_putchar (alp);
+		alp++;
 
-	_putchar('\n');
+	}
+	_putchar ('\n');
+
 }
