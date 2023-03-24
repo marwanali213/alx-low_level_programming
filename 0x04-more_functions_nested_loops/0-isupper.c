bf40aa25be1b0bc3920cc622ed348a669ce66ad8
Marwan Ali <96815791+marwanali213@users.noreply.github.com>
2023-03-25 00:29:49 +0200

    Update 0-isupper.c

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _isupper - uppercase letters
+ * @c: char to check
+ * Return: 1 for upper letter or 0 for any else
+ * Return 0 or 1
+ */
+int _isupper(int c)
+{
+	if (c >= 65 && c <= 90)
+	{
+	return (1);
+	}
+	return (0);
+}
