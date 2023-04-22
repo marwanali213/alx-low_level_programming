c5aacdc498e73c8fccf7c8c1fbb3c19ee8322f80
Marwan Ali <96815791+marwanali213@users.noreply.github.com>
2023-04-22 02:11:45 +0200

    Update 3-print_all.c

--- 0x10-variadic_functions/3-print_all.c
+++ 0x10-variadic_functions/3-print_all.c
@@ -1,95 +1,48 @@
 #include "variadic_functions.h"
-#include <stdio.h>
-#include <stdarg.h>
-#include <stdlib.h>
 
 /**
-* print_char - prints a char
-* @vargs: argument
-* Return: void
-*/
-void print_char(va_list vargs)
-{
-printf("%c", va_arg(vargs, int));
-}
-
-/**
-* print_integer - prints an integer
-* @vargs: argument
-* Return: void
-*/
-void print_integer(va_list vargs)
-{
-printf("%d", va_arg(vargs, int));
-}
-
-/**
-* print_float - prints a float
-* @vargs: argument
-* Return: void
-*/
-void print_float(va_list vargs)
-{
-printf("%f", va_arg(vargs, double));
-}
-
-/**
-* print_string - prints a string
-* @vargs: argument
-* Return: void
-*/
-void print_string(va_list vargs)
-{
-char *ch;
-ch = va_arg(vargs, char *);
-if (ch == NULL)
-{
-printf("(nil)");
-return;
-}
-printf("%s", ch);
-}
-
-/**
-* print_all - a function prints all arguments
-* @format: the format
-* Return: void
-*/
-
+ * print_all - prints anything
+ * @format: list of types of arguments passed to the function
+ */
 void print_all(const char * const format, ...)
 {
-int i, j;
-char *str = "";
-va_list args;
+	int i = 0;
+	char *str, *sep = "";
 
-form p[] = {
-{"c", print_char},
-{"i", print_integer},
-{"f", print_float},
-{"s", print_string},
-{NULL, NULL},
-};
+	va_list list;
 
+	va_start(list, format);
 
-va_start(args, format);
+	if (format)
+	{
+		while (format[i])
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", sep, va_arg(list, int));
+					break;
+				case 'i':
+					printf("%s%d", sep, va_arg(list, int));
+					break;
+				case 'f':
+					printf("%s%f", sep, va_arg(list, double));
+					break;
+				case 's':
+					str = va_arg(list, char *);
+					if (!str)
+						str = "(nil)";
+					printf("%s%s", sep, str);
+					break;
+				default:
+					i++;
+					continue;
+			}
+			sep = ", ";
+			i++;
+		}
+	}
 
-i = 0;
-while (format && format[i])
-{
-j = 0;
-while (p[j].f)
-{
-if (*(p[j].f) == format[i])
-{
-printf("%s", str);
-p[j].func(args);
-str = ", ";
-}
-j++;
-}
-i++;
-}
-
-va_end(args);
-printf("\n");
+	printf("\n");
+	va_end(list);
 }
